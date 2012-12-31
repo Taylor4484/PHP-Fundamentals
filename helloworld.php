66f1141c6f454249f6ffff38f68aa64d59b99b0a
Taylor McCaslin <Taylor4484@gmail.com>
2012-12-31 03:21:03 -0600

    Initial Commit

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>My Website</title>
+	</head>
+	<body>
+		<h1>My Website</h1>
+		<?php
+		
+		$name = "Taylor";
+		
+		echo "Hello $name. How are you doing today $name?"; 
+		?>
+								
+	</body> 
+</html>
